2b1617ee5d3127930f5fd8852a1a6a9b9bfccb41
張憲政 <jeff891029@gmail.com>
2024-02-27 20:43:22 +0800

    第三節上課

--- 20240227/11272009.py
+++ 20240227/11272009.py
@@ -1,1 +1,33 @@
-print("Hello World")
+import feedparser
+
+# 設定要爬取的網址
+url = "https://news.pts.org.tw/xml/newsfeed.xml"
+
+# 使用 feedparser 解析 RSS feed
+feed = feedparser.parse(url)
+
+# 輸出 feed 的標題和項目數量
+print("Feed title:", feed.feed.title)
+print("Number of entries:", len(feed.entries))
+
+# 遍歷每一個 'entry'
+for entry in feed.entries:
+    
+    #幫我確認標題是否有台中 如果有請將標題印出來
+    if "台中" in entry.title:
+        print(entry.title)
+#幫我把列印出來的標題依序存入一個excel檔案中
+import openpyxl
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.title = "News"
+sheet['A1'] = "Title"
+i = 2
+for entry in feed.entries:
+    if "台中" in entry.title:
+        sheet['A'+str(i)] = entry.title
+        i += 1
+wb.save("news.xlsx")
+print("Done!")
+
+
