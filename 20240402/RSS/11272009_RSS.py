45169a2b64f4fc4ec14b32a540a2175a7fbd486a
張憲政 <160596690+xianz90567459@users.noreply.github.com>
2024-04-02 10:42:57 +0000

    RSS

---
+++ 20240402/RSS/11272009_RSS.py
@@ -0,0 +1,21 @@
+##https://www.cwa.gov.tw/V8/C/S/eservice/rss.html
+##https://www.cwa.gov.tw/rss/forecast/36_01.xml
+
+import os
+import requests
+
+# 設定目標資料夾
+folder = '20240402/RSS'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+# 下載檔案
+for i in range(1, 23):
+    num_str = str(i).zfill(2)  # 轉換為兩位數的字串
+    url = f'https://www.cwa.gov.tw/rss/forecast/36_{num_str}.xml'
+    response = requests.get(url)
+    with open(os.path.join(folder, f'36_{num_str}.xml'), 'wb') as f:
+        f.write(response.content)
+
+print('下載完成')
+
